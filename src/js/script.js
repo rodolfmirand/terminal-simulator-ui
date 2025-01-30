550198688d46325615476d30e3d6953845385862
Rodolfo Oliveira Miranda <rodolfo.miranda@ourobranco.mg.gov.br>
2025-01-30 11:37:48 -0300

    modify js

--- src/js/script.js
+++ src/js/script.js
@@ -5,48 +5,57 @@ document.addEventListener('keydown', async (event) => {
         const commandInput = document.getElementById('command')
 
         const commandLine = document.createElement('div')
-        const resultLine = document.createElement('div')
         commandLine.classList.add('command_line')
 
         const commandLineParagraph = document.createElement('p')
         const resultLineParagraph = document.createElement('p')
         const commands = commandInput.value.split(" ")
 
+        const path = document.getElementById('directory').textContent
+        const directory = document.getElementById('directory')
+
         try {
-            const requestText = await request(commands[0], commands)
+            const response = await request(commands[0], commands, path)
+            console.log(response)
 
             commandLineParagraph.textContent = document.getElementById('directory').textContent + commandInput.value
-            resultLineParagraph.innerHTML = requestText.replace(/\n/g, "<br>")
+            resultLineParagraph.innerHTML = response.message.replace(/\n/g, "<br>")
 
             commandLine.appendChild(commandLineParagraph)
             commandLine.appendChild(resultLineParagraph)
 
             document.getElementById('terminal').insertBefore(commandLine, document.querySelector('.input_line'))
+            directory.textContent = response.path
         } catch (error) {
             console.error("Erro ao processar comando:", error)
         }
-
         commandInput.value = ""
     }
+
 })
 
-async function request(command, args) {
+async function request(command, args, path) {
     const url = `http://localhost:8080/${command}`
 
     try {
         const response = await fetch(url, {
             method: 'POST',
-            headers: { 'Content-Type': 'application/json',
+            body: JSON.stringify({
+                'path': path,
+                'args': args
+            }),
+            headers: {
+                'Content-Type': 'application/json',
                 'Accept': 'application/json'
-             }
+            }
         })
 
         if (!response.ok) {
             throw new Error(`Erro: ${response.status}`)
         }
 
-        const text = await response.text()
-        return text
+        const json = await response.json()
+        return json
     } catch (error) {
         console.error(error.message)
         return "Erro ao executar comando."
