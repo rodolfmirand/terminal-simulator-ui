ce15bddfa1494c9306160e8f9bb76825fe8def2f
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-02-05 17:20:07 -0300

    split commands

--- src/js/script.js
+++ src/js/script.js
@@ -9,23 +9,36 @@ document.addEventListener('keydown', async (event) => {
 
         const commandLineParagraph = document.createElement('p')
         const resultLineParagraph = document.createElement('p')
+        resultLineParagraph.classList.add('resultParagraph')
+        
         const commands = commandInput.value.split(" ")
 
         const path = document.getElementById('directory').textContent
         const directory = document.getElementById('directory')
 
         try {
-            const response = await request(commands[0], commands, path)
-            console.log(response)
-
             commandLineParagraph.textContent = document.getElementById('directory').textContent + ">" + commandInput.value
-            resultLineParagraph.innerHTML = response.message.replace(/\n/g, "<br>")
+            var response;
+            switch (commands[0]) {
+                case 'tree':
+                    response = await request('tree', commands, path)
+                    resultLineParagraph.innerText = response.replace(/ {4}/g, '\t')
+                    break;
+                default:
+                    response = await request(commands[0], commands, path)
+                    
+                    resultLineParagraph.innerHTML = response.message.replace(/\n/g, "<br>")
+
+
+                    directory.textContent = response.path
+            }
+            
 
             commandLine.appendChild(commandLineParagraph)
             commandLine.appendChild(resultLineParagraph)
 
             document.getElementById('terminal').insertBefore(commandLine, document.querySelector('.input_line'))
-            directory.textContent = response.path
+
         } catch (error) {
             console.error("Erro ao processar comando:", error)
         }
@@ -54,8 +67,13 @@ async function request(command, args, path) {
             throw new Error(`Erro: ${response.status}`)
         }
 
-        const json = await response.json()
-        return json
+        if (command === 'tree') {
+            const text = await response.text()
+            return text
+        } else {
+            const json = await response.json()
+            return json
+        }
     } catch (error) {
         console.error(error.message)
         return "Erro ao executar comando."
